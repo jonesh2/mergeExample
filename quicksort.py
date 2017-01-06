51053f53284c22bf0cb7985afd6cbf5c1f31f0bc
jonesh2 <jonesh2@carleton.edu>
2017-01-06 11:05:55 -0600

    Added quicksort function with no helper

--- quicksort.py
+++ quicksort.py
@@ -1,4 +1,13 @@
+quickSort(aList, start, end)
+    if start < end:                            # If there are two or more elements...
+        split = partition(aList, start, end)    # ... partition the sublist...
+        quicksort(aList, start, split-1)        # ... and sort both halves.
+        quicksort(aList, split+1, end)
+    else:
+        return
+
+
 def main():
     myList = [3, 2, 5, 1, 9, 0, 0, 4, 45, 12]
-    quickSort(myList)
+    quickSort(myList, 0, len(myList))
     print(myList)
