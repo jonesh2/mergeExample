c186017cb9e4109f5f26e080bf6fcd462ab1721c
jonesh2 <jonesh2@carleton.edu>
2017-01-06 11:17:29 -0600

    added part of partition and quicksort wrong

--- quicksort.py
+++ quicksort.py
@@ -1,5 +1,10 @@
-
 def quickSort(aList):
+    if start < end:                             # If there are two or more elements...
+        split = partition(aList, start, end)    # ... partition the sublist...
+        quicksort(aList, start, split-1)        # ... and sort both halves.
+        quicksort(aList, split+1, end)
+    else:
+        return
 
 def partition(list, start, end):
     pivot = list[end]                          # Partition around the last value
@@ -8,12 +13,24 @@ def partition(list, start, end):
 
     done = 0
 
-        
+    while not done:                            # Until all elements are partitioned...
+
+      while not done:                        # Until we find an out of place element...
+          bottom = bottom+1                  # ... move the bottom up.
+
+          if bottom == top:                  # If we hit the top...
+              done = 1                       # ... we are done.
+              break
+
+          if list[bottom] > pivot:           # Is the bottom out of place?
+              list[top] = list[bottom]       # Then put it at the top...
+              break                          # ... and start searching from the top.
+
     list[top] = pivot                          # Put the pivot in its place.
     return top                                 # Return the split point
 
 
 def main():
-    myList = [3, 2, 5, 1, 9, 0, 0, 4, 45, 12]
+    myList = [3, 2, 5, 1, 9, 0, 0, 1, 45, 12]
     quickSort(myList)
     print(myList)
